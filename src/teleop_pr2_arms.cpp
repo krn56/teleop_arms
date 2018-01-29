f5c0306d17f831d2686607f30a5095e6726afa68
krn56 <karanpatelkrn@gmail.com>
2018-01-29 16:30:43 -0500

    telelop working

--- src/teleop_pr2_arms.cpp
+++ src/teleop_pr2_arms.cpp
@@ -18,11 +18,24 @@ private:
     geometry_msgs::PoseStamped cmd;
 
     ros::NodeHandle n_;
-    ros::Publisher pose_pub;
+    ros::Publisher pose_pub_;
 
 public:
     void init(){
+        //header
+        cmd.header.frame_id = "/torso_lift_link";
 
+        cmd.pose.position.x = 0.9;
+        cmd.pose.position.y = 0.2;
+        cmd.pose.position.z = -0.3;
+        cmd.pose.orientation.x = -0.00244781865415;
+        cmd.pose.orientation.y = -0.548220284495;
+        cmd.pose.orientation.z = 0.00145617884538;
+        cmd.pose.orientation.w = 0.836329126239;
+
+        pose_pub_ = n_.advertise<geometry_msgs::PoseStamped>("r_cart/command_pose",1);
+
+        ros::NodeHandle n_private("~");
     }
 
     ~TeleopPR2Keyboard(){}
@@ -33,7 +46,8 @@ int kfd = 0;
 struct termios cooked, raw;
 
 void quit(int sig){
-
+    tcsetattr(kfd, TCSANOW, &cooked);
+    exit(0);
 }
 
 int main(int argc, char** argv){
@@ -50,5 +64,75 @@ int main(int argc, char** argv){
 }
 
 void TeleopPR2Keyboard::keyboardLoop(){
+    char c;
+    bool dirty = false;
 
+    // get the console in raw mode
+    tcgetattr(kfd, &cooked);
+    memcpy(&raw, &cooked, sizeof(struct termios));
+    raw.c_lflag &=~ (ICANON | ECHO);
+
+    // setting a new line, then end of file
+    raw.c_cc[VEOL] = 1;
+    raw.c_cc[VEOF] = 2;
+    tcsetattr(kfd, TCSANOW, &raw);
+
+    puts("Reading from keyboard");
+    puts("------------------------");
+    puts("Use 'WS' to forward/back");
+    puts("Use 'AD' to left/right");
+    puts("Use 'QE' to up/down");
+
+    for(;;){
+        // get the next event from the keyboard
+        if(read(kfd, &c, 1) < 0){
+            perror("read():");
+            exit(-1);
+        }
+
+        switch (c) {
+        // walking
+        case KEYCODE_W:
+            cmd.pose.position.x = cmd.pose.position.x+0.1;
+            dirty = true;
+            break;
+        case KEYCODE_S:
+            cmd.pose.position.x = cmd.pose.position.x-0.1;
+            dirty = true;
+            break;
+        case KEYCODE_A:
+            cmd.pose.position.y = cmd.pose.position.y+0.1;
+            dirty = true;
+            break;
+        case KEYCODE_D:
+            cmd.pose.position.y = cmd.pose.position.y-0.1;
+            dirty = true;
+            break;
+        case KEYCODE_Q:
+            cmd.pose.position.z = cmd.pose.position.z+0.1;
+            dirty = true;
+            break;
+        case KEYCODE_E:
+            cmd.pose.position.z = cmd.pose.position.z-0.1;
+            dirty = true;
+            break;
+        }
+
+        if (dirty == true){
+            pose_pub_.publish(cmd);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
